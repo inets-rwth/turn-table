a7be7ff675ca1d007847764bf9e1df7c2c23693c
Julian Arnold <julian@elitecoding.org>
2016-02-18 12:02:44 +0100

    new API for turntables

--- firmware/Turntable.ino
+++ firmware/Turntable.ino
@@ -1,79 +1,116 @@
-// pin functions on Arduino
+// pin functions  on Arduino
 #define stp 5
 #define dir 4
 #define MS1 11
 #define MS2 10
 #define MS3 8
 #define EN  12
-#define LED 13
+#define OTT1 2
+#define OTT2 7
+
+// transmission ratio 1:5 or 1:10, 200 steps, 16 microsteps;
+#define TRANSMISSION 5
+
+#define SPR 200*16*TRANSMISSION
+
+//per trigger event table should rotate by 1.125 deg
+#define TRIG_STEP_WIDTH 10*TRANSMISSION
 
 // global variables
-int angle = 5;
-int steps;
 int x;
 int y;
+int val;
+double delvar;
+char serialBuffer[128];
 
 void setup() {
-  pinMode(stp, OUTPUT);
-  pinMode(dir, OUTPUT);
-  pinMode(MS1, OUTPUT);
-  pinMode(MS2, OUTPUT);
-  pinMode(MS3, OUTPUT);
-  pinMode(EN, OUTPUT);
-  pinMode(LED, OUTPUT);
+    pinMode(stp, OUTPUT);
+    pinMode(dir, OUTPUT);
+    pinMode(MS1, OUTPUT);
+    pinMode(MS2, OUTPUT);
+    pinMode(MS3, OUTPUT);
+    pinMode(EN, OUTPUT);
+    pinMode(OTT1, INPUT_PULLUP); //From other Turntable
+    pinMode(OTT2, OUTPUT); //To other Turntable
 
-  digitalWrite(EN, HIGH);
-  
-  // 16 microsteps
-  digitalWrite(MS1, HIGH);
-  digitalWrite(MS2, HIGH);
-  digitalWrite(MS3, HIGH); 
-  
-  analogWrite(LED, 50);
-  
-  Serial.begin(9600); // Open Serial connection
-  Serial.println("Ready");
-  Serial.println(); 
-  
-  // Pull enable pin low to set FETs active and allow motor control
-  digitalWrite(EN, LOW); 
+    digitalWrite(EN, HIGH);
+    digitalWrite(OTT2, HIGH);
+
+    // 16 microsteps
+    digitalWrite(MS1, HIGH);
+    digitalWrite(MS2, HIGH);
+    digitalWrite(MS3, HIGH);
+
+    Serial.begin(9600); // Open Serial connection
+    Serial.println("Ready");
+    Serial.println("Steps per Revolution:");
+    Serial.println(SPR);
+
+    Serial.println();
+
+    // Pull enable pin low to set FETs active and allow motor control
+    digitalWrite(EN, LOW);
 }
+
 
 void loop() {
-  if (Serial.available()) {
-    angle = Serial.parseInt();
-    
-    if (angle < 0) {
-      digitalWrite(dir, LOW);
-      angle = abs(angle);
+    if (Serial.available()) {
+
+        byte num = Serial.readBytesUntil('\n', serialBuffer, 128);
+        if(num == 0) {
+            return;
+        }
+
+        if(strcmp(serialBuffer, "spr", 3) == 0) {
+            Serial.println(SPR);
+        }
+
+        if(strcmp(serialBuffer, "move", 4) == 0) {
+            steps = Serial.parseInt();
+            if (steps < 0) {
+                digitalWrite(dir, LOW);
+                steps = abs(steps);
+            }
+            else {
+                digitalWrite(dir, HIGH);
+            }
+            if (steps != 0) {
+                Step(steps);
+                Serial.println("OK");
+            }
+        }
+
+        if(strcmp(serialBuffer, "trig", 4) == 0) {
+            digitalWrite(OTT2, LOW);
+            delay(1); // 1ms
+            digitalWrite(OTT2, HIGH);
+            Serial.println("OK");
+        }
+
     }
-    else digitalWrite(dir, HIGH);
-	
-	// transmission ration 5; 200 steps, 16 microsteps;
-    steps = angle * 44.44; 
-    
-	// allow only half step positions
-	// steps = steps - (steps % 8);
-	
-    if (steps != 0) {
-      Step();	  
-      Serial.println("done"); 
+    else {
+        val = digitalRead(OTT1);
+        if (val == 0) {
+            Step(TRIG_STEP_WIDTH);
+            delay(2); //make sure to not catch the same trigger again
+        }
     }
-  }
-  else delay(10);
 }
 
-
-void Step(){  
-  for (x = 1; x < steps; x++)  {
-    digitalWrite(stp, HIGH); //Trigger one step forward
-    delay(1.5);
-    digitalWrite(stp, LOW);
-    if ((x < 480 && x <= steps/2) || (x > steps - 480 && x > steps/2)) delay(0.5);
-    if ((x < 240 && x <= steps/2) || (x > steps - 240 && x > steps/2)) delay(0.5);
-    if ((x < 160 && x <= steps/2) || (x > steps - 160 && x > steps/2)) delay(0.5);
-    if ((x < 80 && x <= steps/2) || (x > steps - 80 && x > steps/2)) delay(1);
-    if (x % 160 == 100) analogWrite(LED, 255);
-    if (x % 160 == 0) analogWrite(LED, 50);
-  } 
+void Step(int steps)
+{
+    delvar = 6000;
+    for (x = 1; x <= steps; x++)  {
+        digitalWrite(stp, HIGH); //Trigger one step forward
+        delayMicroseconds(delvar);
+        digitalWrite(stp, LOW);
+        delayMicroseconds(delvar);
+        if (delvar > 150 && x < steps/2) {
+            delvar -= 2*delvar/(4*x+1);
+        }
+        if ((steps > 1752 && x > steps - 876) || (steps <= 1752 && x >= steps/2)) {
+            delvar -= 2*delvar/(4*(x-steps-1)+1);
+        }
+    }
 }
+
